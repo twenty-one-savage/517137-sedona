5cb31d77dd64b870fbaf8325cd412186304bb5c6
twenty-one-savage <Muravtsev21@gmail.com>
2019-05-26 19:50:46 +0300

    Final

--- gulpfile.js
+++ gulpfile.js
@@ -101,6 +101,7 @@ gulp.task("server", function () {
   gulp.watch("source/less/**/*.less", gulp.series("css"));
   gulp.watch("source/img/*-sprite.svg", gulp.series("sprite","html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html","refresh"));
+  gulp.watch("source/js/*.js", gulp.series("jsminify","refresh"));
 });
 
 gulp.task ("refresh", function (done) {
--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -3,7 +3,11 @@ var pageHeader = document.querySelector(".page-header");
 var iconClose = document.querySelector(".main-nav__close");
 var iconOpen = document.querySelector(".burger__button");
 
+navMain.classList.remove("main-nav--opened");
+navMain.classList.add("main-nav--closed");
 pageHeader.classList.remove("page-header--nojs");
+iconClose.classList.remove("nojs-close");
+iconOpen.classList.remove("nojs-burger__button");
 
 iconOpen.addEventListener("click", function() {
   if (navMain.classList.contains("main-nav--closed")) {
